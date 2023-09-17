89720b47a97124d6f453ebd8c6df7aae7cff0227
NorikaU <norikaupd@gmail.com>
2023-09-17 05:15:36 -0400

    added filter, calendar, search components

--- touch-grass/src/components/Filter.js
+++ touch-grass/src/components/Filter.js
@@ -14,22 +14,66 @@ function Filter() {
     setSelectedOption(e.target.value);
   };
 
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState('');
+
+  const addItem = () => {
+    if (newItem.trim() !== '') {
+      setItems([...items, newItem]);
+      setNewItem('');
+    }
+  };
+
+  const removeItem = (index) => {
+    const updatedItems = [...items];
+    updatedItems.splice(index, 1);
+    setItems(updatedItems);
+  };
+
+  const filter = (index) => {
+  };
+
+  
   return (
     <div className="react-dropdown">
-      <h2>Dropdown Menu Example</h2>
-      
-      <select value={selectedOption} onChange={handleSelectChange}>
-        <option value="">Filters</option>
-        <option value="option1">Concert</option>
-        <option value="option2">Sports</option>
-        <option value="option3">Pop-up</option>
-        <option value="option4">Carnival</option>
-        <option value="option5">19+</option>
-        <option value="option6">Women only</option>
-        <option value="option7">BIPOC</option>
+      <button className="Filter" onClick={filter} >
+            Music</button>
+      <button className="Filter" onClick={filter} >
+            Outdoors</button>
+      <button className="Filter" onClick={filter} >
+            Food</button>
+      <button className="Filter" onClick={filter} >
+            Sports</button>
+      <button className="Filter" onClick={filter} >
+            Show</button>
+    <div class="dropdown">  
+      <select  class="dropdown__items" value={newItem} onChange={(e) => setNewItem(e.target.value)} onClick = {addItem}>
+        <label class="dropdown__face" for="dropdown">
+          <div class="dropdown__arrow"></div>
+        </label> 
+        <option value="" class="dropdown__items">View all filters</option>
+        <option value="Music" class="dropdown__items">Music</option>
+        <option value="Outdoors" class="dropdown__items">Outdoors</option>
+        <option value="Food" class="dropdown__items">Food</option>
+        <option value="Sports" class="dropdown__items">Sports</option>
+        <option value="Show" class="dropdown__items">Show</option>
+        <option value="Pop-up" class="dropdown__items">Pop-up</option>
+        <option value="Carnival" class="dropdown__items">Carnival</option>
+        <option value="19+" class="dropdown__items"> 19+</option>
+        <option value="Women" class="dropdown__items">Women</option>
+        <option value="BIPOC" class="dropdown__items">BIPOC</option>
       </select>
-      <p>Selected Option: {selectedOption}</p>
+      <ul>
+        {items.map((item, index) => (
+          <p>
+            <button className="Filter" onClick={() => removeItem(index) } >
+            {item}</button>
+          </p>
+        ))}
+      </ul>
+      </div>
     </div>
+    
   );
 }
 
